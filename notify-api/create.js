48be84d40f3960d6da993bafabfc56cc653d1104
sobhanniroula <sobhan_niroula@yahoo.com>
2019-07-09 00:29:28 +0300

    created API endpoint

--- notify-api/create.js
+++ notify-api/create.js
@@ -20,6 +20,28 @@ export function main(event, context, callback) {
 
     dynamoDb.put(params, (error, data) => {
         // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
 
-    })
+        // Return status code 500 on error
+        if(error) {
+            const response = {
+                statusCode: 500,
+                headers: headers,
+                body: JSON.stringify({ status: false })
+            };
+            callback(null, response);
+            return;
+        }
+
+        // Return status code 200 and the newly created item
+        const response = {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify(params.Item)
+        };
+        callback(null, response);
+    });
 }
