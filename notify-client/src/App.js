e7d85af7fef64233dfb957a474ec80ec17208acb
sobhanniroula <sobhan_niroula@yahoo.com>
2019-07-19 00:50:42 +0300

    Logout implementation

--- notify-client/src/App.js
+++ notify-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
@@ -6,7 +6,31 @@ import Routes from "./Routes";
 import "./App.css";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({
+      isAuthenticated: authenticated
+    });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  }
+
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <div className="App">
         <Navbar fluid collapseOnSelect className="navbar">
@@ -18,16 +42,21 @@ export default class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <LinkContainer to="/signup">
-                <NavItem>Signup</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <NavItem>Login</NavItem>
-              </LinkContainer>
+              {this.state.isAuthenticated 
+                ? <NavItem onClick={this.handleLogout}>LogOut</NavItem>
+                : <Fragment>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </Fragment>
+              } 
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
